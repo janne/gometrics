9ca64f081979fa15b39add7d3fa169ef1ce2ec44
Jan Andersson <jan.andersson@gmail.com>
2013-04-02 14:25:54 +0200

    Add "Fork me on Github" ribbon

--- readme.go
+++ readme.go
@@ -17,8 +17,9 @@ func ReadmeHandler(res http.ResponseWriter, req *http.Request) {
 		w := bufio.NewWriter(res)
 		w.WriteString("<html>")
 		w.WriteString(`<head><link href="http://kevinburke.bitbucket.org/markdowncss/markdown.css" rel="stylesheet"></link></head>`)
+		w.WriteString(`<body><a href="https://github.com/janne/gometrics"><img style="position: absolute; top: 0; right: 30px; border: 0;" src="https://github.com/jamesflorentino/fork-ribbons/raw/master/ribbons/red-white.png" alt="Fork me on GitHub"></a>`)
 		parser.Markdown(file, markdown.ToHTML(w))
-		w.WriteString("</html>")
+		w.WriteString("</body></html>")
 		w.Flush()
 	}
 }
