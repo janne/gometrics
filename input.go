76a5c5378c4d94cc44aeb31b6ca9a1786bfa4128
Jan Andersson <jan.andersson@gmail.com>
2013-04-02 15:21:43 +0200

    Unmarshal POSTed JSON data

--- input.go
+++ input.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
+type Metric struct {
+	Key   string
+	Value float32
+}
+
 func InputHandler(res http.ResponseWriter, req *http.Request) {
-	http.Error(res, "Invalid request", 400)
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(res, err.Error(), 400)
+		return
+	}
+
+	var metrics []Metric
+	err = json.Unmarshal(body, &metrics)
+	if err != nil {
+		http.Error(res, err.Error(), 400)
+		return
+	}
+	for _, m := range metrics {
+		fmt.Fprintln(res, m.Key, "=", m.Value)
+	}
 }
