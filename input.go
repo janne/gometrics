4edc8abc6544d7451276ba9c7604cf6cfa07a132
Jan Andersson <jan.andersson@gmail.com>
2013-04-03 11:00:19 +0200

    Setup Postgres

--- input.go
+++ input.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/bmizerany/pq"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -33,6 +36,27 @@ func InputHandler(res http.ResponseWriter, req *http.Request) {
 			http.Error(res, err.Error(), 400)
 			return
 		}
+		db_url := os.Getenv("DATABASE_URL")
+		db, err := sql.Open("postgres", db_url)
+		if err != nil {
+			http.Error(res, err.Error(), 500)
+			return
+		}
+		rows, err := db.Query("select * from metrics")
+		if err != nil {
+		  http.Error(res, err.Error(), 500)
+		  return
+		}
+		var fields []interface{}
+		for rows.Next() {
+		  rows.Scan(fields...)
+		  err := rows.Scan()
+		  if err != nil {
+			  http.Error(res, err.Error(), 500)
+			  return
+		  }
+		}
+		//fmt.Fprintf(rows, err)
 		fmt.Fprintf(res, "%v,%v,%v\n", t.Format(time.RFC3339), m.Key, m.Value)
 	}
 }
