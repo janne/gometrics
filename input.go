f4474ccddb0f07f06917da381311a0fb3084063c
Jan Andersson <jan.andersson@gmail.com>
2013-04-03 11:48:25 +0200

    Make proper INSERT

--- input.go
+++ input.go
@@ -3,18 +3,39 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"github.com/bmizerany/pq"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"time"
+	//"time"
 )
 
 type Metric struct {
 	Time  string
 	Key   string
 	Value float32
+}
+
+func dbConnect() (db *sql.DB, err error) {
+	dbUrl := os.Getenv("DATABASE_URL")
+	source, err := pq.ParseURL(dbUrl)
+	if err != nil {
+		db, err = sql.Open("postgres", source)
+	}
+	return
+}
+
+func dbInsert(db *sql.DB, query string, args ...interface{}) (err error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return
+	}
+	_, err = tx.Exec(query, args)
+	if err != nil {
+		return
+	}
+	err = tx.Commit()
+	return
 }
 
 func InputHandler(res http.ResponseWriter, req *http.Request) {
@@ -30,38 +51,23 @@ func InputHandler(res http.ResponseWriter, req *http.Request) {
 		http.Error(res, err.Error(), 400)
 		return
 	}
+
+	db, err := dbConnect()
+	if err != nil {
+		http.Error(res, err.Error(), 400)
+		return
+	}
+
 	for _, m := range metrics {
-		t, err := time.Parse(time.RFC3339, m.Time)
-		if err != nil {
-			http.Error(res, err.Error(), 400)
-			return
-		}
-		dbUrl := os.Getenv("DATABASE_URL")
-		dataSourceName, err := pq.ParseURL(dbUrl)
-		if err != nil {
-			http.Error(res, err.Error(), 500)
-			return
-		}
-		db, err := sql.Open("postgres", dataSourceName)
+		//t, err := time.Parse(time.RFC3339, m.Time)
+		//if err != nil {
+		//	http.Error(res, err.Error(), 400)
+		//	return
+		//}
+		// err = dbInsert(db, "INSERT INTO metrics (time, key, value) VALUES (t.Format(time.RFC3339), m.Key, m.Value)")
+		err = dbInsert(db, "INSERT INTO metrics (key, value) VALUES (?, ?)", m.Key, m.Value)
 		if err != nil {
 			http.Error(res, err.Error(), 500)
-			return
 		}
-		rows, err := db.Query("select * from metrics")
-		if err != nil {
-		  http.Error(res, err.Error(), 500)
-		  return
-		}
-		var fields []interface{}
-		for rows.Next() {
-		  rows.Scan(fields...)
-		  err := rows.Scan()
-		  if err != nil {
-			  http.Error(res, err.Error(), 500)
-			  return
-		  }
-		}
-		//fmt.Fprintf(rows, err)
-		fmt.Fprintf(res, "%v,%v,%v\n", t.Format(time.RFC3339), m.Key, m.Value)
 	}
 }
